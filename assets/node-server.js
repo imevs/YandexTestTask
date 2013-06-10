2c36e229cf99961d91eb03134f90e360ab53f7d2
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-06-11 02:01:08 +0400

    task #4 Presentator (deploy to heroku)

--- assets/node-server.js
+++ assets/node-server.js
@@ -3,5 +3,5 @@ var connect = require('connect')
     , fs   = require('fs')
     , app = connect().use(connect.static(__dirname + '/../'));
 
-http.createServer(app).listen(80);
+http.createServer(app).listen(3000);
 fs.writeFileSync(__dirname + '/pid.txt', process.pid, 'utf-8');
