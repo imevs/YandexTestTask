19a1e554ccf03d07f1ceb0824c0bd448385c33f8
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-06-30 19:30:05 +0400

    task #4 Presentator (load images info from remote host)

--- tasks/4.js
+++ tasks/4.js
@@ -3,13 +3,12 @@ var Presentator = (function (w, d, $) {
     var statics = {
         instances: [],
         currentPresentation: null,
-        addToPool: function(obj) {
+        addToPool: function (obj) {
             statics.instances.push(obj);
         },
-        initHotKeys: function() {
-            var self = this;
-            self.instances.length == 1 && $(d).keyup(function(event) {
-                var activePresentation = self.currentPresentation;
+        initHotKeys: function () {
+            $(d).keyup(function (event) {
+                var activePresentation = statics.currentPresentation;
                 if (!activePresentation) return;
 
                 if (event.keyCode == 37 || event.keyCode == 38) {
@@ -20,20 +19,20 @@ var Presentator = (function (w, d, $) {
                 }
             });
         },
-        disableFullScreenForAll: function(current) {
-            var self = this;
-            self.currentPresentation = current;
-            for (var i = 0; i < self.instances.length; i++) {
-                if (self.instances[i] !== self.currentPresentation) {
-                    self.instances[i].fullscreenDisable();
+        disableFullScreenForAll: function (current) {
+            statics.currentPresentation = current;
+            for (var i = 0; i < statics.instances.length; i++) {
+                if (statics.instances[i] !== statics.currentPresentation) {
+                    statics.instances[i].fullscreenDisable();
                 }
             }
         },
-        init: function() {
-            $(function() {
-                $('.presentator').each(function() {
+        init: function () {
+            $(function () {
+                $('.presentator').each(function () {
                     Presentator({context: $(this)});
                 });
+                statics.initHotKeys();
             });
         }
     };
@@ -42,15 +41,16 @@ var Presentator = (function (w, d, $) {
         if (!(this instanceof Presentator)) {
             return new Presentator(params);
         }
-        var me = this;
+        var me = this,
+            tmpImage = $('<img>'),
+            log = params.logger || function (msg) {
+                w.console.log(msg);
+            };
 
         me.statics = statics;
 
         statics.addToPool(me);
 
-        var log = params.logger || function(msg) {
-            w.console.log(msg);
-        };
         me.$context = $(params.context);
         me.url = params.url || me.$context.data('url') || 'images.json';
         me.prevBtn = params.prevBtn || '.prev';
@@ -62,53 +62,69 @@ var Presentator = (function (w, d, $) {
         me.slidesCount = 0;
         me.thumbnailClass = params.thumbnailClass || 'span4';
         me.fullscreenClass = params.fullscreenClass || 'span10';
-
         me.slides = params.slides;
-
-        var tmpImage = $('<img>');
+        me.dataRoot = me.$context.data('root') || 'items';
+        me.dataSrcKey = me.$context.data('key') || '';
 
         me.init = function () {
             me.$prevBtn.click(function () { me.back(); });
             me.$nextBtn.click(function () { me.forward(); });
-            me.$img.click(function() { me.fullscreenToggle(); });
+            me.$img.click(function () { me.fullscreenToggle(); });
+            me.$img.addClass('imgthumb');
+            me.setActiveSlide(0);
             $.getJSON(me.url).done(function (data) {
-                me.slides = data.items;
+                me.slides = $.map(data[me.dataRoot], function (item) {
+                    return me.dataSrcKey ? item[me.dataSrcKey] : item;
+                });
                 me.slidesCount = me.slides.length;
                 me.setActiveSlide(0);
+            }).error(function () {
+                log('Origin ' + d.location.origin + ' is not allowed by Access-Control-Allow-Origin.');
             });
-            statics.initHotKeys();
         };
-
-        me.fullscreenToggle = function() {
+        me.fullscreenToggle = function () {
             statics.disableFullScreenForAll(me);
+            me.$img.toggleClass('imgthumb');
+            me.$img.toggleClass('imgfull');
             me.$context.toggleClass(me.thumbnailClass);
             me.$context.toggleClass(me.fullscreenClass);
         };
-        me.fullscreenDisable = function() {
+        me.fullscreenDisable = function () {
+            me.$img.addClass('imgthumb');
+            me.$img.removeClass('imgfull');
             me.$context.addClass(me.thumbnailClass);
             me.$context.removeClass(me.fullscreenClass);
         };
         me.loadSlide = function (id) {
+            if (!me.slides) return;
+
             var imageSrc = me.slides[id];
             tmpImage.one('load', function () {
                 log('loaded ' + id);
                 me.$img.attr('src', imageSrc);
-            });
-            tmpImage.attr('src', imageSrc);
+            }).attr('src', imageSrc);
         };
         me.setActiveSlide = function (number) {
             if (number == me.currentSlide) return;
 
-            me.currentSlide = number;
-            me.loadSlide(me.currentSlide);
-            if (me.currentSlide == me.slidesCount - 1) {
-                me.$nextBtn.addClass('disabled');
-            } else if (me.currentSlide == 0) {
-                me.$prevBtn.addClass('disabled');
+            if (me.slidesCount) {
+                me.currentSlide = number;
+                me.loadSlide(me.currentSlide);
+
+                if (me.currentSlide >= me.slidesCount - 1) {
+                    me.$nextBtn.addClass('disabled');
+                } else if (me.currentSlide == 0) {
+                    me.$nextBtn.removeClass('disabled');
+                    me.$prevBtn.addClass('disabled');
+                } else {
+                    me.$prevBtn.removeClass('disabled');
+                    me.$nextBtn.removeClass('disabled');
+                }
             } else {
-                me.$prevBtn.removeClass('disabled');
-                me.$nextBtn.removeClass('disabled');
+                me.$prevBtn.addClass('disabled');
+                me.$nextBtn.addClass('disabled');
             }
+
         };
         me.forward = function () {
             var newSlideNumber = Math.min(me.currentSlide + 1, me.slidesCount - 1);
